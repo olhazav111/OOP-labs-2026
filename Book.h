a06cb72dfc5fb05347803cd8ec8bac7dc689a555
ZavyaletsOlha <olazav200@gmail.com>
2026-03-16 21:12:51 +0200

    Added copy constructor to Book class

--- Book.h
+++ Book.h
@@ -11,15 +11,28 @@ private:
     std::string isbn;
     bool available;
 
+    static int bookCount;
+
 public:
     Book(std::string t = "Unknown", std::string a = "Unknown", std::string i = "000");
     Book(std::string t);
+
+    Book(const Book& other);
+    Book(Book&& other);
 
     ~Book();
 
     void borrowBook();
     void returnBook();
-    void displayInfo();
+    void displayInfo() const;
+
+    static int getBookCount();
+
+    bool operator!();
+    bool operator==(const Book& other) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Book& book);
+    friend std::istream& operator>>(std::istream& is, Book& book);
 };
 
 #endif
