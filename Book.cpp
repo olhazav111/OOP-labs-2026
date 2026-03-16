4a6b38ad7bf48a24d15b7ed175f173e9a0939f55
ZavyaletsOlha <olazav200@gmail.com>
2026-03-16 21:13:09 +0200

    Implemented move constructor and usage of this pointer

--- Book.cpp
+++ Book.cpp
@@ -1,12 +1,39 @@
 #include "Book.h"
 
+int Book::bookCount = 0;
+
 Book::Book(std::string t, std::string a, std::string i)
         : title(t), author(a), isbn(i), available(true)
 {
+    bookCount++;
     std::cout << "Book created: " << title << std::endl;
 }
 
 Book::Book(std::string t) : Book(t, "Unknown", "000") {}
+
+Book::Book(const Book& other)
+{
+    title = other.title;
+    author = other.author;
+    isbn = other.isbn;
+    available = other.available;
+
+    std::cout << "Book copied: " << title << std::endl;
+}
+
+Book::Book(Book&& other)
+{
+    title = std::move(other.title);
+    author = std::move(other.author);
+    isbn = std::move(other.isbn);
+    available = other.available;
+
+    other.title = "Moved";
+    other.author = "";
+    other.isbn = "";
+
+    std::cout << "Book moved: " << title << std::endl;
+}
 
 Book::~Book()
 {
@@ -15,18 +42,45 @@ Book::~Book()
 
 void Book::borrowBook()
 {
-    available = false;
+    this->available = false;
 }
 
 void Book::returnBook()
 {
-    available = true;
+    this->available = true;
 }
 
-void Book::displayInfo()
+void Book::displayInfo() const
 {
     std::cout << "Book: " << title
               << " | Author: " << author
               << " | Available: " << (available ? "Yes" : "No")
               << std::endl;
+}
+
+int Book::getBookCount()
+{
+    return bookCount;
+}
+
+bool Book::operator!()
+{
+    return !available;
+}
+
+bool Book::operator==(const Book& other) const
+{
+    return isbn == other.isbn;
+}
+
+std::ostream& operator<<(std::ostream& os, const Book& book)
+{
+    os << book.title << " | " << book.author << " | " << book.isbn;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Book& book)
+{
+    is >> book.title >> book.author >> book.isbn;
+    return is;
 }
